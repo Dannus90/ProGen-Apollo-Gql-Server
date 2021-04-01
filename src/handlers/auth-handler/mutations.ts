42c84163cb1e1811686e5bd3cc7cb7b94b5f4169
Daniel Persson <persson.daniel.1990@gmail.com>
2021-04-02 00:50:08 +0200

    Fix register resolver

--- src/handlers/auth-handler/mutations.ts
+++ src/handlers/auth-handler/mutations.ts
@@ -9,11 +9,15 @@
  import { AuthenticationMutationRootToRegisterUserResolver } from "../../types/TypesGraphQL";
  
  export interface AuthenticationMutations {
+  AuthenticationMutationRoot: {
      registerUser: AuthenticationMutationRootToRegisterUserResolver
+    }
   }
  
  export const authMutations: AuthenticationMutations = {
+  AuthenticationMutationRoot: {
    registerUser: async(_, body, { api }: Context) => {
      return await api.registerUser(body.input?.email, body.input?.password)
    }
+  }
  }
