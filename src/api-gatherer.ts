4d03dae0b5592d9c117abb6d7124ca3cdabd0437
Daniel Persson <persson.daniel.1990@gmail.com>
2021-04-05 12:53:32 +0200

    Fix typo

--- src/api-gatherer.ts
+++ src/api-gatherer.ts
@@ -14,7 +14,7 @@ export interface GetApiMethods {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   logoutUser: (authorization: string) => Promise<any>;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  refreshToken: (uthorization: string, input: GQLRefreshTokenInput | undefined) => Promise<any>;
+  refreshToken: (authorization: string, input: GQLRefreshTokenInput | undefined) => Promise<any>;
 }
 
 export const getApiMethods = (): GetApiMethods => {
