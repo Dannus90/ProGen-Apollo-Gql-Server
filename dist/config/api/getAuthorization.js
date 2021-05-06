14bdd085c6aee1f33d6b469d9c9fd4822e1aa51a
Daniel Persson <persson.daniel.1990@gmail.com>
2021-05-06 21:21:31 +0200

    remove dist folder

--- dist/config/api/getAuthorization.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getAuthorization = void 0;
-const getAuthorization = (req) => req.headers.authorization ?? "";
-exports.getAuthorization = getAuthorization;
-//# sourceMappingURL=getAuthorization.js.map
